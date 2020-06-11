be4154b487658fa79514c480f96be37ef73275ee
cbatula <45442655+cbatula@users.noreply.github.com>
2020-06-11 16:29:11 -0700

    Create faucet.sh

---
+++ installScripts/faucet.sh
@@ -0,0 +1,5 @@
+sudo apt-get install curl gnupg apt-transport-https lsb-release
+echo "deb https://packagecloud.io/faucetsdn/faucet/$(lsb_release -si | awk '{print tolower($0)}')/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/faucet.list
+curl -L https://packagecloud.io/faucetsdn/faucet/gpgkey | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install faucet-all-in-one
